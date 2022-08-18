d97e64a68ecd6e59704e58a092932aed60598f4a
Dayane <dayaneac@hotmail.com>
2022-08-18 17:51:55 -0300

    versão com inserir cartas

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
 let quantidade =  Number(prompt ('Com quantas cartas quer jogar? (4, 6, 8, 10, 12 ou 14)'));
 
 while (quantidade !== 4 && quantidade !== 6 && quantidade !== 8 && quantidade !== 10 && quantidade !== 12 && quantidade !== 14 ) {
-    quantidade = Number(prompt ('Com quantas cartas quer jogar? (4, 6, 8, 10, 12 ou 14)'));
+quantidade = Number(prompt ('Com quantas cartas quer jogar? (4, 6, 8, 10, 12 ou 14)')); 
 }
+
+inserirCartas (quantidade);
+
 
 function inserirCartas (quantidade) {
 
     const lista = document.querySelector ('ul');
+    let array = [];
 
-    for (i=3; i<quantidade; i++) {
+    for (let i=0; i<quantidade; i++) {
 
-        let item = `<li class = "carta">
+        array.push (`
+                    <li class = "carta carta${i}">
                         <img src = "Arquivos Úteis - Projeto 04 - Parrot Card Game/parrot.png" />
-                    </li>`
-
-    lista.innerHTML = lista.innerHTML + item;
+                    </li>
+                    `);       
     }
 
-    minhaArray.sort(comparador);
-    function comparador() { 
-        return Math.random() - 0.5; 
-    }
+    lista.innerHTML = lista.innerHTML + array; 
+
+    array.sort(comparador); 
+   
+function comparador() { 
+	return Math.random() - 0.5; 
+} 
 }
 
 
